954b75bbb8edac31873fcf1ce7ca2dbbf12ae15f
rainbow23 <info@rainbow23.jp>
2024-08-14 16:40:17 +0900

    gravity レイアウトパターンをすべて追加

--- app/src/main/java/com/example/customview/MainActivity.kt
+++ app/src/main/java/com/example/customview/MainActivity.kt
@@ -7,7 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main4)
         //setContentView(binding.root)
     }
 }
